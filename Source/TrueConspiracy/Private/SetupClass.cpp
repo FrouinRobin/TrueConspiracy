3c52ef961443d9aefd1efa43261450273f7542ec
FrouinRobin <rfrouin@gaming.tech>
2025-03-07 17:33:53 +0100

    ProjectSetup

---
+++ Source/TrueConspiracy/Private/SetupClass.cpp
@@ -0,0 +1,12 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+//Created to add Private File
+
+#include "SetupClass.h"
+
+SetupClass::SetupClass()
+{
+}
+
+SetupClass::~SetupClass()
+{
+}
---
+++ Source/TrueConspiracy/TrueConspiracy.cpp
@@ -0,0 +1,9 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "TrueConspiracy.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, TrueConspiracy, "TrueConspiracy" );
+
+DEFINE_LOG_CATEGORY(LogTrueConspiracy)
+ 
